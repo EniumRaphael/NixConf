f84076468ea9ff4c3813088fe391d0a3b7e30482
Raphaël <35407363+EniumRaphael@users.noreply.github.com>
2024-10-03 20:03:53 +0200

    update: here's config

---
+++ nixos/configuration.nix
@@ -0,0 +1,211 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  services.xserver.videoDrivers = ["nvidia"];
+
+  hardware.nvidia = {
+    modesetting.enable = true;
+    powerManagement.enable = false;
+    powerManagement.finegrained = false;
+    open = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+};
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Enable documentation
+
+  documentation = {
+    enable = true;
+    man.enable = true;
+    dev.enable = true;
+  };
+
+  # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+	services.xserver = {
+		enable = true;
+	};
+	services.greetd = {
+		enable = true;
+		settings = {
+			default_session = {
+				command = "${pkgs.greetd.tuigreet}/bin/tuigreet --remember --remember-user-session --time --cmd Hyprland";
+			};
+		};
+	};
+	sound.enable = true;
+	security.rtkit.enable = true;
+	services.pipewire = {
+		enable = true;
+		alsa.enable = true;
+		alsa.support32Bit = true;
+		pulse.enable = true;
+		jack.enable = true;
+	};
+
+	nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.raphael = {
+    isNormalUser = true;
+    description = "raphael";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+	home-manager
+	];
+  };
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+	ags
+	cava
+	dunst
+	gtk3
+	pkg-config
+	fastfetch
+	git
+	home-manager
+	imagemagick
+	jq
+	kdePackages.kio-extras
+	kdePackages.kio-fuse
+	kdePackages.qtsvg
+	kdePackages.qtwayland
+	kitty
+	libnotify
+	nerdfonts
+	networkmanager
+	pamixer
+	pavucontrol
+	pipewire
+	playerctl
+	rofi
+	swaylock
+	swaylock-fancy
+	swww
+	linux-manual
+	man
+	man-pages-posix
+	man-pages
+	unzip
+	waybar
+	wireplumber
+	wl-clipboard
+	wlogout
+	wofi
+	zsh
+  	vim
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+
+	   programs.steam = {
+		 enable = true;
+		 remotePlay.openFirewall = true;
+		 dedicatedServer.openFirewall = true;
+	   };
+	programs.firefox.enable = true;
+	programs.thunar = {
+		enable = true;
+		plugins = with pkgs.xfce; [
+		  	thunar-archive-plugin
+	  		thunar-volman
+		];
+	};
+	services.gvfs.enable = true; # Mount, trash, and other functionalities
+	services.tumbler.enable = true; # Thumbnail support for images
+
+	programs.zsh.enable = true;
+	users.defaultUserShell = pkgs.zsh;
+	users.users.raphael.useDefaultShell = true;
+
+	programs.hyprland = {
+		enable = true;
+		xwayland.enable = true;
+	};
+
+	environment.sessionVariables = {
+		WLR_NO_HARDWARE_CURSORS = "1";
+		NIXOS_OZONE_WL = "1";
+	};
+
+	hardware = {
+		opengl.enable = true;
+	};
+
+	xdg.portal = {
+		enable = true;
+		extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+	};
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+	system.stateVersion = "24.05"; # Did you read the comment?
+
+}
